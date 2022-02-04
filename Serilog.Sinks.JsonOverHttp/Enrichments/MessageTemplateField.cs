ddde011c5a03c3a2da40c930db6496aa175adea7
IFYates <ian@iyates.co.uk>
2022-02-04 08:55:52 +0000

    Can choose for MessageTemplate to only be exposed if it isn't static

--- Serilog.Sinks.JsonOverHttp/Enrichments/MessageTemplateField.cs
+++ Serilog.Sinks.JsonOverHttp/Enrichments/MessageTemplateField.cs
@@ -1,14 +1,26 @@
 ﻿using Serilog.Core;
 using Serilog.Events;
+using Serilog.Parsing;
+using System.Linq;
 
 namespace Serilog.Sinks.JsonOverHttp.Enrichers
 {
     public class MessageTemplateField : ILogEventEnricher
     {
+        public bool OnlyIfDifferent { get; }
+
+        public MessageTemplateField(bool onlyIfDifferent = false)
+        {
+            OnlyIfDifferent=onlyIfDifferent;
+        }
+
         public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
         {
-            var prop = propertyFactory.CreateProperty("MessageTemplate", logEvent.MessageTemplate.Text);
-            logEvent.AddPropertyIfAbsent(prop);
+            if (!OnlyIfDifferent || logEvent.MessageTemplate.Tokens.Count() > 1 || logEvent.MessageTemplate.Tokens.First() is PropertyToken)
+            {
+                var prop = propertyFactory.CreateProperty("MessageTemplate", logEvent.MessageTemplate.Text);
+                logEvent.AddPropertyIfAbsent(prop);
+            }
         }
     }
 }
